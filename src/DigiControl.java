9f3bb54f6f53bf9adabcf7fabb64fc30ab5974e6
Jose Maria <jmateosmartin@safareyese.es>
2024-01-24 18:44:55 +0100

    Cuarto

--- src/DigiControl.java
+++ src/DigiControl.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class DigiControl {
     private static final int[] ValoresConstantes = {1,2,4,8,5,10,9,7,3,6};
-    public void IngresarDigitos() {
+    public void IngresarDigitosControl() {
         Scanner leer = new Scanner(System.in);
-        System.out.println("Ingrese los 4 digitos del banco: ");
+        System.out.print("Ingrese los 4 digitos del banco: ");
         String BancoDigitos = leer.next();
-        System.out.println("Ingrese los 4 digitos de la sucursal: ");
+        System.out.print("Ingrese los 4 digitos de la sucursal: ");
         String SucursalDigitos = leer.next();
-        System.out.println("Ingrese los 10 digitos de la cuenta: ");
+        System.out.print("Ingrese los 10 digitos de la cuenta: ");
         String CuentaDigitos = leer.next();
 
         String DC1 = GenerarControlDigitos("00" + BancoDigitos + SucursalDigitos);
@@ -32,8 +32,38 @@ public class DigiControl {
             return Integer.toString(resultado);
         } else if (resultado == 10) {
             return "1";
-        } else {
+        } else if (resultado == 11) {
             return "0";
+        }
+        return input;
+    }
+
+    public void VerificacionControDigitos() {
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Ingrese los 4 digitos del banco: ");
+        String BancoDigitos = leer.next();
+        System.out.print("Ingrese los 4 digitos de la sucursal: ");
+        String SucursalDigitos = leer.next();
+        System.out.print("Ingrese los 2 digitos de control: ");
+        String ControlDigitos = leer.next();
+        System.out.print("Ingrese los 10 digitos de la cuenta: ");
+        String CuentaDigitos = leer.next();
+
+        ValidarDigitosControl(BancoDigitos, SucursalDigitos, ControlDigitos, CuentaDigitos);
+    }
+
+    public void ValidarDigitosControl(String BancoDigitos, String SucursalDigitos, String ControlDigitos, String CuentaDigitos) {
+        String DC1 = GenerarControlDigitos("00" + BancoDigitos + SucursalDigitos);
+        String DC2 = GenerarControlDigitos(CuentaDigitos);
+
+        String DigitsCalculados = DC1 + DC2;
+
+        if (ControlDigitos.equals(DigitsCalculados)) {
+            System.out.println("Número de cuenta a validar: " + BancoDigitos + " " + SucursalDigitos + " " + ControlDigitos + " " + CuentaDigitos);
+            System.out.println("Dígitos calculados: " + DigitsCalculados + " Verificación: Correcta");
+        } else {
+            System.out.println("Número de cuenta a validar: " + BancoDigitos + " " + SucursalDigitos + " " + ControlDigitos + " " + CuentaDigitos);
+            System.out.println("Dígitos calculados: " + DigitsCalculados + " Verificación: Incorrecta");
         }
     }
 }
--- src/PilotDigits.java
+++ src/PilotDigits.java
@@ -4,20 +4,32 @@ public class PilotDigits {
         Scanner leer = new Scanner(System.in);
         boolean menu = true;
         DigiControl objeto = new DigiControl();
+        DigiIBAN objeto2 = new DigiIBAN();
         while(menu) {
-            System.out.println("Menú de opciones");
-            System.out.println("================");
+            System.out.println("\033[1m\t  Menú de Opciones\033[0m");
+            System.out.println("\033[1m\t  ================\033[0m");
             System.out.println("1) Generar digitos de control");
             System.out.println("2) Verificar digitos de control");
             System.out.println("3) Generar digitos del IBAN");
             System.out.println("4) Verificar digitos del IBAN");
             System.out.println("5) Salir");
-            System.out.println("Opción: ");
+            System.out.print("Opción: ");
             int opcion = leer.nextInt();
-            // Estructura switch para manejar la opción seleccionada
             switch (opcion) {
                 case 1:
-                    objeto.IngresarDigitos();
+                    objeto.IngresarDigitosControl();
+                    break;
+                case 2:
+                    objeto.VerificacionControDigitos();
+                    break;
+                case 3:
+                    objeto2.IngresarDigitosIban();
+                    break;
+                case 4:
+                    objeto2.VerificacionIBAN();
+                    break;
+                case 5:
+                    menu = false;
                     break;
             }
         }
